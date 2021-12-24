8487a35294c561b49d8f70f5d2dbae58ad50f888
Aybatu <aybatu.kerkukluoglu@gmail.com>
2021-12-24 00:46:32 +0000

    Initial Commit

---
+++ SwiftUI_P5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_P5
+//
+//  Created by Aybatu Kerkukluoglu on 24/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_P5/SwiftUI_P5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_P5App.swift
+//  SwiftUI_P5
+//
+//  Created by Aybatu Kerkukluoglu on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_P5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
